e4c3bea4150b251313623006eeaab4f34c605aad
bspell1 <bspell1@gmail.com>
2012-11-06 10:03:33 -0600

    Added the VS extension package

---
+++ Samples/Library/Library/Class.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Library
+{
+   public sealed class Class
+   {
+   }
+}
---
+++ Vsix/Package/ProjectFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace NuBuild.Vsix
+{
+   [Guid(GuidList.guidProjectFactoryString)]
+   public sealed class ProjectFactory
+   {
+      public ProjectFactory ()
+      {
+      }
+   }
+}
