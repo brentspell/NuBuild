dead7aa4e1ec1c17139031afad0b9ed36e1896ec
bspell1 <bspell1@gmail.com>
2013-06-04 09:32:26 -0500

    1.4 release

--- Version.cs
+++ Version.cs
@@ -21,5 +21,5 @@
 // System References
 using System.Reflection;
 // Project References
-[assembly:AssemblyVersion("1.3.0.0")]
-[assembly:AssemblyFileVersion("1.3.0.0")]
+[assembly:AssemblyVersion("1.4.0.0")]
+[assembly:AssemblyFileVersion("1.4.0.0")]
