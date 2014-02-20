a7d19d122b6f378a1bccad328db6d3ed5efed6b9
lmagyar <lmagyargithub@gmail.com>
2014-02-20 03:28:39 +0100

    version 2.1

--- Version.cs
+++ Version.cs
@@ -21,5 +21,5 @@
 // System References
 using System.Reflection;
 // Project References
-[assembly:AssemblyVersion("2.0.0.0")]
-[assembly:AssemblyFileVersion("2.0.0.0")]
+[assembly:AssemblyVersion("2.1.0.0")]
+[assembly:AssemblyFileVersion("2.1.0.0")]
